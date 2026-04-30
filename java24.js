df7ea3f32f419a08efdbf6b2792ad9a2378baddf
maverick732 <202513369@btech.ph.education>
2026-04-30 10:36:38 +0800

    Prime Number Sum Calculator

---
+++ java24.js
@@ -0,0 +1,17 @@
+const sumPrimes = (num) => {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+console.log(sumPrimes(10));
